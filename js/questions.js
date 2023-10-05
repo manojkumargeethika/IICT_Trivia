597872d14dd9842de1d99c890dfe255690ce4f87
Geethika Manojkumar <manojkumargeethika@gmail.com>
2023-10-05 04:10:34 -0400

    first_commit

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How tall is the Battle Monument?",
+    answer: "130 ft",
+    options: [
+      "150 ft",
+      "300 ft",
+      "200 ft",
+      "130 ft"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The height of the Battle Monument matches the height of which other famous monument?",
+    answer: "Column of Marcus Aurelius",
+    options: [
+      "Arc de Triomphe",
+      "Statue of Liberty",
+      "Column of Marcus Aurelius",
+      "Washington Monument"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What was the year with the highest number of visitors to the monument?",
+    answer: "1947",
+    options: [
+      "1956",
+      "1947",
+      "1944",
+      "1989"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How much did it cost to visit the top of the monument?",
+    answer: "10 cents",
+    options: [
+      "12 cents",
+      "20 cents",
+      "10 cents",
+      "15 cents"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the sculpture of George at the top holding in his hands? ",
+    answer: "telescope",
+    options: [
+      "telescope",
+      "bird",
+      "gun",
+      "book"
+    ]
+  },
+ 
+];
